b8058dc94d1ecccb467f5bf3eb17931945250321
Azeem <azeem.asim01@gmail.com>
2023-02-27 22:55:44 +0500

    sticker smash

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -1,18 +1,53 @@
-import { StyleSheet, Image, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Image, View } from "react-native";
+import React from "react";
+import Animated, {
+  BounceIn,
+  useAnimatedStyle,
+  useSharedValue,
+  useAnimatedGestureHandler,
+  withSpring,
+} from "react-native-reanimated";
 
-const EmojiSticker = ({imageSize, stickerSource}) => {
+import { TapGestureHandler } from "react-native-gesture-handler";
+
+const EmojiSticker = ({ imageSize, stickerSource }) => {
+  const AnimatedImage = Animated.createAnimatedComponent(Image);
+  const scaleImage = useSharedValue(imageSize);
+
+  // const onDoubleTap = useAnimatedGestureHandler({
+  //   onActive: () => {
+  //     if (scaleImage.value) {
+  //       // scaleImage.value = scaleImage.value * 2
+  //       scaleImage.value *= 2;
+  //     }
+  //   },
+  // });
+
+  const onDoubleTap = ()=>{
+      alert("run") ;
+  }
+
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      width: withSpring(scaleImage.value),
+      height: withSpring(scaleImage.value),
+    };
+  });
+
   return (
-    <View style={{top: -350}}>
-      <Image 
-        source={stickerSource} 
-        resizeMode="contain" 
-        style={{width: imageSize, height: imageSize}}
+    <View style={{ top: -350 }}>
+      <TapGestureHandler onGestureEvent={onDoubleTap} numberOfTaps={2}>
+        <AnimatedImage
+          entering={BounceIn}
+          source={stickerSource}
+          resizeMode="contain"
+          style={[imageStyle, { width: imageSize, height: imageSize }]}
         />
+      </TapGestureHandler>
     </View>
-  )
-}
+  );
+};
 
-export default EmojiSticker
+export default EmojiSticker;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
